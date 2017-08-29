4facaf270c1f3bf809f8c1b83745b205027355f4
Emil Öhgren <emil@emil.ohgren>
2017-08-29 15:02:20 +0200

    add logging.conf

--- hooks/neutron_contexts.py
+++ hooks/neutron_contexts.py
@@ -180,3 +180,22 @@ def get_shared_secret():
         with open(_path, 'r') as secret_file:
             secret = secret_file.read().strip()
     return secret
+
+class LoggingContext(OSContextGenerator):
+
+    def __call__(self):
+        ctxt = {}
+        ctxt['json_logs'] = config('json-logs')
+        debug = config('debug')
+        if debug:
+            ctxt['root_level'] = 'DEBUG'
+        log_level = config('log-level')
+        log_level_accepted_params = ['WARNING', 'INFO', 'DEBUG', 'ERROR']
+        if log_level in log_level_accepted_params:
+            ctxt['log_level'] = config('log-level')
+        else:
+            log("log-level must be one of the following states "
+                "(WARNING, INFO, DEBUG, ERROR) keeping the current state.")
+            ctxt['log_level'] = None
+
+        return ctxt
